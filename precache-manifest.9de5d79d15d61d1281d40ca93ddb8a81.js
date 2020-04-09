ef95306a0ba9cf535509462ff98fd74eeb43dc47
gen143 <johncenareddy1997@gmail.com>
2020-04-09 21:59:07 +0530

    Updates

---
+++ precache-manifest.9de5d79d15d61d1281d40ca93ddb8a81.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb81f588fe0ecb24d6064471f017be0",
+    "url": "/DeployReactCorona/index.html"
+  },
+  {
+    "revision": "059f45bd95534ccbda7c",
+    "url": "/DeployReactCorona/static/css/2.cc80b6a4.chunk.css"
+  },
+  {
+    "revision": "e264c1c43c198e179808",
+    "url": "/DeployReactCorona/static/css/main.d57658d4.chunk.css"
+  },
+  {
+    "revision": "059f45bd95534ccbda7c",
+    "url": "/DeployReactCorona/static/js/2.af8dee19.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/DeployReactCorona/static/js/2.af8dee19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e264c1c43c198e179808",
+    "url": "/DeployReactCorona/static/js/main.e8c8c9ae.chunk.js"
+  },
+  {
+    "revision": "977c6b4cbf1fffd45661",
+    "url": "/DeployReactCorona/static/js/runtime-main.2ae43c7e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/DeployReactCorona/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/DeployReactCorona/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/DeployReactCorona/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/DeployReactCorona/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/DeployReactCorona/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DeployReactCorona/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "554689337070fbc6a00028bee348bc4e",
+    "url": "/DeployReactCorona/static/media/me.55468933.jpg"
+  },
+  {
+    "revision": "9f422f78076a90edc90e590e5cc52056",
+    "url": "/DeployReactCorona/static/media/namast.9f422f78.jpg"
+  }
+]);
